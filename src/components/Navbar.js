964cb19ed3639b40f7dcd0ab87f64cf474b5716d
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-22 08:45:28 +0000

    Add Navbar component and modified it

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css'
+
+const NavBar = () => {
+
+  return (
+    <Navbar expand="md" className={styles.NavBar} fixed='top'>
+      <Container>
+        <Navbar.Brand ><img src={logo} alt='logo' height='45'/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link >
+                <i class='fas fa-home'></i> Home</Nav.Link>
+            <Nav.Link >
+            <i class='fas fa-sign-in-alt'></i> Sign In</Nav.Link>
+            <Nav.Link >
+            <i class='fas fa-user-plus'></i> Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
